2d724c1eaf049671bf132a0bee11278f548a4a85
arvieore <arvieore@gmail.com>
2024-03-05 19:00:19 +0800

    Connections
    Connections for Users.

---
+++ WheelyRev/WheelyRev/Controllers/BaseController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WheelyRev.Models;
+using WheelyRev.Repository;
+
+namespace WheelyRev.Controllers
+{
+    public class BaseController : Controller
+    {
+        // GET: Base
+        private WheelyRevEntities _db;  //Connection
+        private BaseRepository<Users> _table;   //Users table
+        public BaseController()
+        {
+            _db = new WheelyRevEntities();
+            _table = new BaseRepository<Users>();
+        }
+    }
+}
